6dc906e696d712831b0aaff80dab2c938054cfa6
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-03-29 02:15:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+let users = [
+    {'id': 0, 'name':'shohan','dept':'cse'},
+    {'id': 1, 'name':'arif','dept':'cse'},
+    {'id': 2, 'name':'sharif','dept':'cse'},
+    {'id': 3, 'name':'niloy','dept':'cse'},
+    {'id': 4, 'name':'walid','dept':'cse'}
+];
+
+app.get('/users', (req, res) => {
+  const search = req.query.search;
+  if(search){
+    const searchedUser = users.filter(user =>user.name.toLocaleLowerCase().includes(search) );
+    res.send(searchedUser);
+  }
+  else{
+      res.send(users);
+  }
+});
+app.get('/users/:id', (req,res)=>{
+    id = req.params.id;
+    res.send(users[id]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
